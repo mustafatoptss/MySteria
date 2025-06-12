5bc6d6522011733f011be2cd2412fdfee2a36c83
mustafatoptss <mustafatoptss80@gmail.com>
2025-06-12 19:05:15 +0300

    sssss

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss() ,// React için
-  ],
+  plugins: [react()],
   server: {
-    host: '0.0.0.0', // Telefonda test için gereklidir
-    port: 5176,       // Dilersen başka port da seçebilirsin
-  }
+    host: '0.0.0.0',
+    port: 5176,
+  },
 })
