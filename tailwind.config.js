80b61d19ddfa61293dc80a0494199aedf573c5d6
mustafatoptss <mustafatoptss80@gmail.com>
2025-06-12 18:46:04 +0300

    aaa

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  corePlugins: {
-    preflight: true // ✅ AÇIK tutun (modern projelerde önerilen)
-  },
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    fontFamily: {
-      sans: ['Roboto', 'sans-serif'], // Varsayılan font
-      poppins: ['Poppins', 'sans-serif'],
-      lora: ['Lora', 'serif'],
-    },
-    fontSize: {
-      base: '1rem',   // 16px (zorunlu sabit)
-      sm: '0.875rem', // 14px
-      lg: '1.125rem', // 18px
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+        lora: ['Lora', 'serif'],
+      },
     },
   },
- 
   plugins: [],
+  corePlugins: {
+    preflight: true, // Temel sıfırlama
+  },
 }
