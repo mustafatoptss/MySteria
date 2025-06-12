0d89875fc39f030cd26af2b54dedce9678c5ce0a
mustafatoptss <mustafatoptss80@gmail.com>
2025-06-12 18:09:46 +0300

    ss

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,20 @@ export default {
   ],
   theme: {
     extend: {
+      fontSize: { // Font boyutlarını sabitle
+        base: '1rem',   // 16px (varsayılan)
+        lg: '1.125rem', // 18px
+        xl: '1.25rem',  // 20px
+      },
       fontFamily: {
         roboto: ['Roboto', 'sans-serif'],
         bebas: ['"Bebas Neue"', 'sans-serif'],
         montserrat: ['Montserrat', 'sans-serif'],
       },
-      
     },
+  },
+  corePlugins: {
+    preflight: false, // Tarayıcı resetlerini KAPAT (önemli!)
   },
   plugins: [],
 }
