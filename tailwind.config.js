99745ad03bec17e164aa114f63e57f7e3bbbf5e8
mustafatoptss <mustafatoptss80@gmail.com>
2025-06-13 01:43:35 +0300

    ss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",     // src klasörü ve altındaki tüm .jsx dosyaları
+      "./pages/**/*.{js,ts,jsx,tsx}", 
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",      // src klasörü ve altındaki tüm .jsx dosyaları
   ],
   theme: {
     extend: {
